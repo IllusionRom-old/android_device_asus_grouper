de9b33a4f3b1e7a4e9677a977d053ad1dd4018b9
Andreas Fried <AndrolinRoot@googlemail.com>
2013-12-15 20:08:27 +0100

    Make Base for Grouper

--- illusion.mk
+++ illusion.mk
@@ -1,3 +1,9 @@
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1280
+TARGET_SCREEN_WIDTH := 720
+
+PRODUCT_RELEASE_NAME := grouper
+
 $(call inherit-product, vendor/illusion/config/common.mk)
 $(call inherit-product, device/asus/grouper/device.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
@@ -8,3 +14,10 @@ PRODUCT_BRAND := Android
 PRODUCT_MODEL := Nexus 7
 PRODUCT_MANUFACTURER := Asus
 PRODUCT_RESTRICT_VENDOR_FILES := false
+
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=occam BUILD_FINGERPRINT="google/occam/mako:4.4.1/KOT49E/907817:user/release-keys" PRIVATE_BUILD_DESC="occam-user 4.4.1 KOT49E 907817 release-keys"
+
+# bootanimation
+PRODUCT_COPY_FILES += \
+        vendor/illusion/bootanimation/Illusion768x768.zip:system/media/bootanimation.zip
